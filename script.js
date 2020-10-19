03750acb1c0f2db6e572d4c392280b5d2da03189
SAYEED <sayeed.705@gmail.com>
2020-10-19 20:30:41 +0530

    added new to top btn

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-$("button").click(function () {
+$("#viewbtn").click(function () {
     $('html,body').animate({
         scrollTop: $("#work-content").offset().top
     },
         'slow');
 });
+
+
+totopbtn = document.getElementById("totopbtn");
+window.onscroll = function () { scrollfunction() };
+function scrollfunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        totopbtn.style.display = "block";
+    }
+    else {
+        totopbtn.style.display = "none";
+    }
+}
+
+function topfunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+}
